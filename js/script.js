0019bb47a58de52f58f9a8d233baff90cb337c24
Olga Kotova <oliktva@gmail.com>
2016-12-16 22:44:04 +0300

    Add scroll effects

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ $(window).on('scroll', function (e) {
       el.removeClass("header--gray");
     }
 })
+
+$(document).ready(function() {
+      $("a.scrollto").click(function () {
+        elementClick = $(this).attr("href")
+        destination = $(elementClick).offset().top;
+        $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+        return false;
+      });
+    });
