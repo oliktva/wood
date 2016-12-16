2d1e73d447c4245a53b75658c6bf0dc451f23c7b
Olga Kotova <oliktva@gmail.com>
2016-12-16 21:38:14 +0300

    upd site.js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 var ghpages = require("gh-pages");
 var path = require("path");
 ghpages.publish(path.join(__dirname, "build"),{
-  repo: "git@github.com:oliktva/wood.git"
+  repo: "https://github.com/oliktva/wood.git"
 }, callback);
